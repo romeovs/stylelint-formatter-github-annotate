d6435e59effc031d4fb06e25946bd15c87f0ee70
Romeo Van Snick <romeovs@gmail.com>
2021-08-16 15:51:55 +0200

    Print message when there were errors

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import stylelint from "stylelint"
 import { startGroup, endGroup } from "@actions/core"
 import { issueCommand } from "@actions/core/lib/command"
 
-function formatter(results: stylelint.LintResult[]): "" {
+function formatter(results: stylelint.LintResult[]): string {
 	startGroup("Lint Annotations")
 
+	let errorCount = 0
 	for (const result of results) {
 		for (const warning of result.warnings) {
 			const { text, severity } = warning
@@ -16,12 +17,20 @@ function formatter(results: stylelint.LintResult[]): "" {
 			}
 
 			issueCommand(severity, properties, text)
+
+			if (severity === "error") {
+				errorCount++
+			}
 		}
 	}
 
 	endGroup()
 
-	return ""
+	if (errorCount === 0) {
+		return ""
+	}
+
+	return `There were ${errorCount} errors\n`
 }
 
 export = formatter
